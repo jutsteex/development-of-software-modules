157d60895379c736be0b248e9279bfba4245da75
Kirill <makarovkostya98@mail.ru>
2023-04-06 00:11:32 +0700

    Add new project

---
+++ Практические/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public TreeViewItem SelectedTreeElem { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem tvItem = (TreeViewItem)sender;
+            
+        }
+
+
+    }
+}
---
+++ Практические/WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Phone
+        {
+            public string Title { get; set; }
+            public string Company { get; set; }
+            public int Price { get; set; }
+        }
+        public List<Phone> phonesList = new List<Phone>
+        {
+           new Phone {Title="iPhone 6S", Company="Apple", Price=54990 },
+           new Phone {Title="Lumia 950", Company="Microsoft", Price=39990 },
+           new Phone {Title="Nexus 5X", Company="Google", Price=29990 }
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            phonesGrid.ItemsSource = phonesList;
+        }
+
+    }
+}
